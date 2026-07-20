36247e3c383a2175708755a7310fd116a247ac51
biohaters26-alt <biohaters26@gmail.com>
2026-07-20 09:06:29 +0600

    Update ProductCard.js

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -73,7 +73,7 @@ export default function ProductCard({ product }) {
         </button>
 
         <div className="mt-2 pt-2 border-t border-white/[0.06]">
-         <OrderActions productName={product.name} layout="col" compareUrl={product.compareUrl} />
+         <OrderActions productName={product.name} layout="col" compareUrl={product.compareUrl} price={product.price} />
             
         </div>
       </div>
